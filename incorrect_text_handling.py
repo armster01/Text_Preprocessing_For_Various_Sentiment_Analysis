4da95fb6eda05430c2339b1af624eb93cdeb288e
ADITTYA SAHA <armstersaha8@gmail.com>
2025-01-04 17:14:06 +0530

    Delete incorrect_text_handling.py

--- incorrect_text_handling.py
+++
@@ -1,6 +0,0 @@
-from textblob import TextBlob
-
-incorrect_text = "ceertain conditionas duriing seceal ggeneratioons aree moodifieed in the saame maner"
-
-textBlb = TextBlob(incorrect_text)
-textBlb.correct().string
